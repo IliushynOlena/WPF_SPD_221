1049504ffec9bfad2b272975b9f85e2d88669e64
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-11-05 13:44:20 +0200

    Data Binding, Data Context

---
+++ 10_DataContext/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10_DataContext
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel ViewModel = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            //Binding
+           //this.DataContext = ViewModel;
+
+        }
+    }
+    public class ViewModel
+    {
+        public User User1 { get; set; }
+        public User User2 { get; set; }
+        public ViewModel()
+        {
+            User1 = new User() { Name = "Bob", Email = "bob@gmail.com" };
+            User2 = new User() { Name = "Tom", Email = "tom@gmail.com" };
+
+        }
+
+    }
+}
---
+++ 11_PhoneBook/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_PhoneBook
+{
+    public class Contact//: INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public bool IsMale { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}  {Surname}";
+        }
+    }
+}
