60071c31253b15668cdd99fabb49a495b285b7bc
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-11-18 15:49:49 +0200

    Command Binding

--- 11_PhoneBook/MainWindow.xaml.cs
+++ 11_PhoneBook/MainWindow.xaml.cs
@@ -29,26 +29,28 @@ namespace _11_PhoneBook
             this.DataContext = model;            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            model.DeleteSelectedContact();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            model.DublicateContact();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            model.DeleteAllContact();
-        }
     }
     class ViewModel
     {
+        //Commands
+        RelayCommand dublicateCommand;
+        RelayCommand removeCommand;
+        RelayCommand clearCommand;
+
+        public ICommand DublicateCommand => dublicateCommand;
+        public ICommand RemoveCommand => removeCommand;
+        public ICommand ClearCommand => clearCommand;
         private ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
         public ViewModel()
         {
+
+            dublicateCommand = new RelayCommand((o) => { DublicateContact(); }, 
+                (o)=> SelectedContact != null);
+            removeCommand = new RelayCommand((o) => { DeleteSelectedContact(); },
+                (o) => SelectedContact != null);
+            clearCommand = new RelayCommand((o) => { contacts.Clear(); }, (o) => contacts.Any());
+
+
             contacts.Add(new Contact() { Name = "Vova", Age = 30, Surname = "Pupkin", Phone = "+3807575828", IsMale = true });
             contacts.Add(new Contact() { Name = "Marusia", Age = 25, Surname = "Shishik", Phone = "+380771244", IsMale = false });
             contacts.Add(new Contact() { Name = "Olga", Age = 33, Surname = "Shelesh", Phone = "+38067285792", IsMale = false });
@@ -70,10 +72,10 @@ namespace _11_PhoneBook
             if(SelectedContact != null)
                 contacts.Add(SelectedContact.Clone());
         }
-        public void DeleteAllContact()
-        {
-            if(contacts.Any())
-                contacts.Clear();
-        }
+        //public void DeleteAllContact()
+        //{
+        //    if(contacts.Any())
+        //        contacts.Clear();
+        //}
     }
 }
